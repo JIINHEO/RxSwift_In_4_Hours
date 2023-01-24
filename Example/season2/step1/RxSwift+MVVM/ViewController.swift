ef1c81de8a912cdb52ecc9f7d3ae5255addc7dcc
jiinheo <gjwldls123@gmail.com>
2023-01-24 17:27:46 +0900

    dipose - 취소

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -83,7 +83,7 @@ class ViewController: UIViewController {
         // 비동기: 현재 작업은 그대로 진행하고 다른 스레드에서 원하는 작업을 비동기적으로 동시에 수행
         // 다른 스레드에서 멀티스레드로 일을 처리한 다음에 그 결과를 비동기적으로 받아서 처리를 함
         
-        downloadJson(MEMBER_LIST_URL)
+        let disposable = downloadJson(MEMBER_LIST_URL)
             .subscribe { event in
                 switch event {
                 case .next(let json):
@@ -95,5 +95,7 @@ class ViewController: UIViewController {
                     break
                 }
             }
+        // 취소시킴
+        disposable.dispose()
     }
 }
