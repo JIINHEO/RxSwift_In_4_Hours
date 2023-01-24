29e67cde521b5fb7fa1469adc9ad62b718b6ce6e
jiinheo <gjwldls123@gmail.com>
2023-01-24 19:20:53 +0900

    menuListViewModle 생성

---
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by jiin heo on 2023/01/24.
+//  Copyright © 2023 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// Model: View를 위한 Model -> ViewModel
+struct Menu {
+    var name: String
+    var price: Int
+    var count: Int
+}
