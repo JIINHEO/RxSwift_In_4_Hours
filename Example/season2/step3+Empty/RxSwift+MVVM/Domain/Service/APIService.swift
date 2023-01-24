33222689ecea4cfe21ed79313821b887fd979ab2
jiinheo <gjwldls123@gmail.com>
2023-01-24 21:04:47 +0900

    API 연결

--- Example/season2/step3+Empty/RxSwift+MVVM/Domain/Service/APIService.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Domain/Service/APIService.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RxSwift
 
 let MenuUrl = "https://firebasestorage.googleapis.com/v0/b/rxswiftin4hours.appspot.com/o/fried_menus.json?alt=media&token=42d5cb7e-8ec4-48f9-bf39-3049e796c936"
 
@@ -26,5 +27,24 @@ class APIService {
             }
             onComplete(.success(data))
         }.resume()
+    }
+    
+    static func fetchAllMenusRx() -> Observable<Data> {
+        return Observable.create() { emmiter in
+            // 서버에서 받아온 모델과 기존 만들어놓은 모델이 다르다
+            // 레거시 코드를 rx로 감싼다
+            fetchAllMenus { result in
+                switch result {
+                case let .success(data):
+                    emmiter.onNext(data)
+                    emmiter.onCompleted()
+                case let .failure(err):
+                    emmiter.onError(err)
+                }
+            }
+            
+            return Disposables.create()
+            
+        }
     }
 }
--- Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -13,9 +13,8 @@ import RxSwift
 // 따라서 viewModel만 testCase 돌리는게 좋다
 // view에서는 버그가 발생할 일이 별로 없다
 class MenuListViewModel {
-    g
     // 데이터 이미 들어갔는데 나중에 subscribe를 했기 때문에 변경 초기값을 갖는 behavior로 변경
-
+    
     var menuObservable = BehaviorSubject<[Menu]>(value: [])
     
     lazy var itemCount = menuObservable.map { $0.map { $0.count}.reduce(0, +) }
@@ -25,14 +24,21 @@ class MenuListViewModel {
     // obserbable 밖에서 값을 control해서 새로운 값을 집어넣어줄 수 있음
     
     init() {
-        let menus: [Menu] = [
-            Menu(id:0, name: "튀김1", price: 100, count: 0),
-            Menu(id:1, name: "튀김2", price: 100, count: 0),
-            Menu(id:2, name: "튀김3", price: 100, count: 0),
-            Menu(id:3, name: "튀김4", price: 100, count: 0),
-        ]
-        
-        menuObservable.onNext(menus)
+        _ = APIService.fetchAllMenusRx()
+            .map { data in
+                struct Response: Decodable {
+                    let menus: [MenuItem]
+                }
+                let response = try! JSONDecoder().decode(Response.self, from: data)
+                return response.menus
+            }
+            .map { menuItems -> [Menu] in
+                return menuItems.map {
+                    Menu.fromMenuItems(id: 0, item: $0)
+                }
+            }
+            .take(1)
+            .bind(to: menuObservable)
     }
     
     func onOrder() {
