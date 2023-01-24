6c2218aa5c38f452bc3be3473f2fc728a939dd67
jiinheo <gjwldls123@gmail.com>
2023-01-24 17:07:14 +0900

    나중에생기는데이터 class 생성 및 return값으로 처리

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -12,6 +12,18 @@ import UIKit
 
 let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
+class 나중에생기는데이터<T> {
+    private let task: (@escaping (T) -> Void) -> Void
+    
+    init(task: @escaping (@escaping (T) -> Void) -> Void) {
+        self.task = task
+    }
+    
+    func 나중에오면(_ f: @escaping (T) -> Void) {
+        task(f)
+    }
+}
+
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
@@ -34,16 +46,19 @@ class ViewController: UIViewController {
     
     // 함수 분리
     func downloadJson(_ url: String) -> 나중에생기는데이터<String?> {
-        // 그렇다면 completion 말고 return값으로 받을 수 없을까?
-        DispatchQueue.global().async {
-            // 문제는 리턴을 못해서 completion을 사용해야함
-            let url = URL(string: MEMBER_LIST_URL)!
-            let data = try! Data(contentsOf: url)
-            let json = String(data: data, encoding: .utf8)
-            DispatchQueue.main.async {
-                // 본 함수가 끝나고 나서 나중에 실행되는 함수여서 escaping을 해준다.
-                // 만약 옵셔널클로저인경우에는 escaping이 default라 안해줘도됨
-                completion(json)
+        return 나중에생기는데이터 { f in
+            // 그렇다면 completion 말고 return값으로 받을 수 없을까?
+            DispatchQueue.global().async {
+                // 문제는 리턴을 못해서 completion을 사용해야함
+                let url = URL(string: MEMBER_LIST_URL)!
+                let data = try! Data(contentsOf: url)
+                let json = String(data: data, encoding: .utf8)
+                
+                DispatchQueue.main.async {
+                    // 본 함수가 끝나고 나서 나중에 실행되는 함수여서 escaping을 해준다.
+                    // 만약 옵셔널클로저인경우에는 escaping이 default라 안해줘도됨
+                    f(json)
+                }
             }
         }
     }
@@ -59,8 +74,9 @@ class ViewController: UIViewController {
         // 비동기: 현재 작업은 그대로 진행하고 다른 스레드에서 원하는 작업을 비동기적으로 동시에 수행
         // 다른 스레드에서 멀티스레드로 일을 처리한 다음에 그 결과를 비동기적으로 받아서 처리를 함
         
-        let json:나중에생기는데이터<String>? = downloadJson(MEMBER_LIST_URL)
-        json.나중에생기면 { json in
+        let json:나중에생기는데이터<String?> = downloadJson(MEMBER_LIST_URL)
+        
+        json.나중에오면 { json in
             self.editView.text = json
             self.setVisibleWithAnimation(self.activityIndicator, false)
         }
